7f50ce071c8f23a4de34723cb12f25a6c68c389f
Erhan Gündüz <erhangndz@live.com>
2024-04-02 16:37:43 +0300

    user register

---
+++ IdentityServer/Multishop.IdentityServer/Controllers/RegistersController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Multishop.IdentityServer.Dtos;
+using Multishop.IdentityServer.Models;
+using System.Threading.Tasks;
+
+namespace Multishop.IdentityServer.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RegistersController : ControllerBase
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public RegistersController(UserManager<ApplicationUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateUser(RegisterDto registerDto)
+        {
+            var user = new ApplicationUser
+            {
+                UserName = registerDto.UserName,
+                Email = registerDto.Email,
+
+            };
+          var result =   await _userManager.CreateAsync(user, registerDto.Password);
+            if (!result.Succeeded)
+            {
+                return BadRequest(result.Errors);
+            }
+
+            return Ok("Kullanıcı Kayıt Edildi");
+        }
+    }
+}
--- IdentityServer/Multishop.IdentityServer/Models/ApplicationUser.cs
+++ IdentityServer/Multishop.IdentityServer/Models/ApplicationUser.cs
@@ -5,5 +5,6 @@ namespace Multishop.IdentityServer.Models
     // Add profile data for application users by adding properties to the ApplicationUser class
     public class ApplicationUser : IdentityUser
     {
+      
     }
 }
