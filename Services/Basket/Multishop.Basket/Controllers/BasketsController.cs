ae03a73cfb62515c18bff8da3feaa5d9e283e694
Erhan Gündüz <erhangndz@live.com>
2024-04-12 12:41:36 +0300

    updated

---
+++ Services/Basket/Multishop.Basket/Controllers/BasketsController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Multishop.Basket.Controllers
+{
+    [Authorize]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BasketsController : ControllerBase
+    {
+    }
+}
+
