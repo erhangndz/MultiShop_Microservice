3412098a28ef00201cfa69cf3ee861f2ae510ca3
Erhan Gündüz <erhangndz@live.com>
2024-04-16 18:27:42 +0300

    Home Index components

---
+++ Frontends/Multishop.WebUI/ViewComponents/Home/_HomeCarousel.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Multishop.WebUI.ViewComponents.Home
+{
+    public class _HomeCarousel:ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
---
+++ Frontends/Multishop.WebUI/ViewComponents/UILayout/_UILayoutHead.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Multishop.WebUI.ViewComponents.UILayout
+{
+    public class _UILayoutHead:ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
