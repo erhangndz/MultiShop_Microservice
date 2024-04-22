455b0da734688978d18277ef95d232799d80f9cf
Erhan Gündüz <erhangndz@live.com>
2024-04-22 20:26:47 +0300

    admin/ create category

---
+++ Frontends/Multishop.WebDTO/DTOs/CatalogDtos/CategoryDtos/UpdateCategoryDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Multishop.WebDTO.DTOs.CatalogDtos.CategoryDtos
+{
+    public class UpdateCategoryDto
+    {
+        public string Id { get; set; }
+        public string CategoryName { get; set; }
+    }
+}
--- Frontends/Multishop.WebUI/Areas/Admin/Controllers/CategoryController.cs
+++ Frontends/Multishop.WebUI/Areas/Admin/Controllers/CategoryController.cs
@@ -1,13 +1,40 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Multishop.WebDTO.DTOs.CatalogDtos.CategoryDtos;
 
 namespace Multishop.WebUI.Areas.Admin.Controllers
 {
     [Area("Admin")]
+    [Route("[area]/[controller]/[action]/{id?}")]
     public class CategoryController : Controller
     {
-        public IActionResult Index()
+        private readonly HttpClient _client;
+
+        public CategoryController(HttpClient client)
+        {
+            client.BaseAddress = new Uri("https://localhost:7060/api/");
+            _client = client;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var values = await _client.GetFromJsonAsync<List<ResultCategoryDto>>("categories");
+            if (values == null)
+            {
+                return View(new List<ResultCategoryDto>());
+            }
+            return View(values);
+        }
+
+        public IActionResult CreateCategory()
         {
             return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateCategory(CreateCategoryDto createCategoryDto)
+        {
+            await _client.PostAsJsonAsync("categories", createCategoryDto);
+            return RedirectToAction("Index");
         }
     }
 }
