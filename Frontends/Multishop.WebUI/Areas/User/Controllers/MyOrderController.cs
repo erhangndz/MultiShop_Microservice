b012261c2665ee1170b82f02a19e1686394a4d9b
Erhan Gündüz <erhangndz@live.com>
2024-05-19 15:59:29 +0300

    spica theme user layout

---
+++ Frontends/Multishop.WebUI/Areas/User/Controllers/MyOrderController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Multishop.WebUI.Areas.User.Controllers
+{
+    [Area("User")]
+    [AllowAnonymous]
+    public class MyOrderController : Controller
+    {
+        public IActionResult OrderList()
+        {
+            return View();
+        }
+    }
+}
---
+++ Frontends/Multishop.WebUI/Areas/User/Controllers/UserLayoutController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Multishop.WebUI.Areas.User.Controllers
+{
+    public class UserLayoutController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
