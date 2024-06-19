8b1090bab19c7c0682ade0d257a26e671cd1ec69
Erhan Gündüz <erhangndz@live.com>
2024-06-19 13:20:12 +0300

    rabbitmq services

---
+++ Services/RabbitMQ/Multishop.RabbitMQApi/Controllers/QMessagesController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Multishop.RabbitMQApi.Services;
+using RabbitMQ.Client;
+using System.Text;
+
+namespace Multishop.RabbitMQApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class QMessagesController(RabbitMQService _service) : ControllerBase
+    {
+
+        [HttpPost]
+        public IActionResult CreateMessage()
+        {
+            var channel = _service.CreateChannel();
+            channel.QueueDeclare("Queue2", false, false, false, null);
+            var messageContent = "Merhaba bugün hava çok sıcak";
+
+            var byteMessage = Encoding.UTF8.GetBytes(messageContent);
+            channel.BasicPublish(exchange: "", routingKey: "Queue2", basicProperties: null, body: byteMessage);
+            return Ok("Mesaj Kuyruğa Alınmıştır.");
+        }
+
+        [HttpGet]
+        public IActionResult ReadMessage()
+        {
+            return Ok();
+        }
+    }
+}
---
+++ Services/RabbitMQ/Multishop.RabbitMQApi/Services/RabbitMQService.cs
@@ -0,0 +1,35 @@
+﻿using RabbitMQ.Client;
+
+namespace Multishop.RabbitMQApi.Services
+{
+    public class RabbitMQService
+    {
+        public IConnection CreateConnection()
+        {
+            var connectionFactory = new ConnectionFactory()
+            {
+                HostName = "localhost",
+
+            };
+            var connection = connectionFactory.CreateConnection();
+            return connection;
+
+           
+        }
+
+        public IModel CreateChannel()
+        {
+            var connection = CreateConnection();
+            var channel = connection.CreateModel();
+            return channel;
+        }
+
+        public void QueueDeclare(string queueName)
+        {
+            var channel = CreateChannel();
+            channel.QueueDeclare(queueName, false, false, false, null);
+        }
+
+
+    }
+}
